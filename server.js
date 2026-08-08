594122ca0ae188e6758b1fd51aec4f0c20331d7e
Residentplay <lucasevanessasousa@gmail.com>
2026-08-08 10:43:26 -0300

    segurança

--- server.js
+++ server.js
@@ -86,10 +86,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
-app.use(cors({
-  origin: "https://projeto-cadastrogeo.onrender.com",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.static(__dirname));
 app.use(cookieParser());
 

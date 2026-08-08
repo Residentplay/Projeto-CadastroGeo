728060c7d329ed9c957eb00847111bc2b1c570ed
Residentplay <lucasevanessasousa@gmail.com>
2026-08-08 08:53:21 -0300

    segurança

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { rateLimit } = require("express-rate-limit");
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 5,
+  skipSuccessfulRequests: true,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   message: {

864e80f5f85f2ab0c27f3bd6827cc0d4491a47e6
Residentplay <lucasevanessasousa@gmail.com>
2026-08-08 08:42:58 -0300

    segurança

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { Pool } = require("pg");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const { rateLimit } = require("express-rate-limit");
 
 
 if (!process.env.JWT_SECRET || process.env.JWT_SECRET.length < 32) {

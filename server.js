977726f5bfc8d1044c80aeda5f182d9a56632b5a
Residentplay <lucasevanessasousa@gmail.com>
2026-08-09 20:16:09 -0300

    atualização automatica do dashboard

--- server.js
+++ server.js
@@ -321,6 +321,16 @@ await pg.query(`
 
 console.log("Tabela PostgreSQL 'usuarios' verificada/criada.");
 
+// LIMPEZA TEMPORÁRIA - remover depois do primeiro deploy
+await pg.query(`
+  DELETE FROM atribuicoes
+  WHERE casa_id NOT IN (
+    SELECT id FROM casas
+  )
+`);
+
+console.log("Atribuições órfãs removidas.");
+
 
 }
 
